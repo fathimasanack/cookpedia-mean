7c150b00e52d4d62149808419ec2c5405b66f42f
fathimasanack <ckfathimasana120@gmail.com>
2024-12-02 16:08:22 +0530

    cook

---
+++ src/app/app.routes.ts
@@ -0,0 +1,44 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SavedRecipeComponent } from './saved-recipe/saved-recipe.component';
+import { ViewRecipeComponent } from './view-recipe/view-recipe.component';
+import { PnfComponent } from './pnf/pnf.component';
+
+export const routes: Routes = [
+    {
+        path:"", component:HomeComponent,title:"Home"
+    },
+    {
+        path:"about", component:AboutComponent,title:"About"
+    },
+    {
+        path:"contact", component:ContactComponent,title:"Contact"
+    },
+    {
+        path:"login", component:LoginComponent,title:"Login"
+    },
+    {
+        path:"register", component:RegisterComponent,title:"Register"
+    },
+    {
+        path:"all-recipes", component:RecipesComponent,title:"All Recipes"
+    },
+    {
+        path:"profile", component:ProfileComponent,title:"Profile"
+    },
+    {
+        path:"save-recipe", component:SavedRecipeComponent,title:"Save Recipes"
+    },
+    {
+        path:"recipe/:id/view", component:ViewRecipeComponent,title:"View Recipe"
+    },
+    {
+        path:"**", component:PnfComponent,title:"Page Not Found"
+    }
+];
